5a3dbfe211230387d4a432a4e745f07dd8ef504f
mhaikalazizan <mhaikalazizan@gmail.com>
2022-04-09 15:02:59 +0800

    Tambah text pada appbar dan body

--- lib/main.dart
+++ lib/main.dart
@@ -7,6 +7,15 @@ void main() {
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(home: Text('Hello!'));
+    // Scaffold design UI dalam app
+    return MaterialApp(
+      home: Scaffold(
+        // letak title dekat top bar app
+        appBar: AppBar(title: Text('My App')),
+
+        // letak text dekat body app
+        body: Text('This is my default text on app!'),
+      ),
+    );
   }
 }
